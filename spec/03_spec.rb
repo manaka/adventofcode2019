5c31360bd4ad14a57944393d668f962cd85430d9
manaka <romeod@gmail.com>
2019-12-03 13:13:09 +0100

    Added 03.01 solution

--- spec/03_spec.rb
+++ spec/03_spec.rb
@@ -5,16 +5,20 @@ require './03_01'
 RSpec.describe CrossedWires, 'intersection_point' do
   context '2 wires' do
     it 'R8,U5,L5,D3 and U7,R6,D4,L4' do
-      c_w = CrossedWires.new 'R8,U5,L5,D3', 'U7,R6,D4,L4'
-      expect(c_w.intersection_point).to eq 6
+      cw = CrossedWires.new 'R8,U5,L5,D3', 'U7,R6,D4,L4'
+      expect(cw.intersection_distance).to eq 6
     end
     it 'R75,D30,R83,U83,L12,D49,R71,U7,L72 and U62,R66,U55,R34,D71,R55,D58,R83' do
-      c_w = CrossedWires.new 'R75,D30,R83,U83,L12,D49,R71,U7,L72', 'U62,R66,U55,R34,D71,R55,D58,R83'
-      expect(c_w.intersection_point).to eq 159
+      cw = CrossedWires.new 'R75,D30,R83,U83,L12,D49,R71,U7,L72', 'U62,R66,U55,R34,D71,R55,D58,R83'
+      expect(cw.intersection_distance).to eq 159
     end
     it 'R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51 and U98,R91,D20,R16,D67,R40,U7,R15,U6,R7' do
-      c_w = CrossedWires.new 'R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51', 'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7'
-      expect(c_w.intersection_point).to eq 159
+      cw = CrossedWires.new 'R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51', 'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7'
+      expect(cw.intersection_distance).to eq 135
+    end
+    it 'U1,U2,R3,U2,L5 and R4,U1,U1,U1,U3,L3,D5' do
+      cw = CrossedWires.new 'U1,U2,R3,U2,L5', 'R4,U1,U1,U1,U3,L3,D5'
+      expect(cw.intersection_distance).to eq 4
     end
 
   end
